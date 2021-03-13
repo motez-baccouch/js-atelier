1c5164b7c5bfef9a690f4c79197eb9e80c6a5e01
motez-baccouch <80569257+motez-baccouch@users.noreply.github.com>
2021-03-13 09:40:47 +0100

    test

---
+++ ex1/script.js
@@ -0,0 +1,22 @@
+var diff=parseInt(prompt("donner le niveau de difficulté 1:facile 2:moyenne 3:difficile"));
+if(diff>3)diff=3;
+var nbr=Math.floor(Math.random()*10);
+var count=Math.abs(diff-4);
+console.log(nbr);
+ 
+function again(){
+    if(confirm("voulez vous repetez le jeu ^^ ?")){location.reload();}
+}
+
+
+while(count!=0){
+        var a=parseInt(prompt("guess the number(between 0-10"));
+        if(a==nbr){alert("great work you guessed it right:"+nbr);
+        again();
+}
+            count--;
+    }
+    alert("sorry for you :'( maybe next time, the number was"+nbr);
+    again();
+
+
